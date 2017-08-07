9034f3f3efe5cc1e8c4193d8f5d344025a759374
赵宏亚 <feiliyaailiya1@icloud.com>
2017-08-07 11:43:43 +0800

    忘记密码开始

---
+++ ForgetPasswordTest/ForgetPasswordTest/ForgetPasswordVC.h
@@ -0,0 +1,13 @@
+//
+//  ForgetPasswordVC.h
+//  ForgetPasswordTest
+//
+//  Created by 赵宏亚 on 2017/8/7.
+//  Copyright © 2017年 赵宏亚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForgetPasswordVC : UIViewController
+
+@end
--- ForgetPasswordTest/ForgetPasswordTest/ViewController.m
+++ ForgetPasswordTest/ForgetPasswordTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ForgetPasswordVC.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"ForgetPasswordStoryboard" bundle:nil];
+    
+    ForgetPasswordVC *forgetPassword = [storyboard instantiateViewControllerWithIdentifier:@"ForgetPasswordVC"];
+    [self.navigationController pushViewController:forgetPassword animated:YES];
 }
 
 
